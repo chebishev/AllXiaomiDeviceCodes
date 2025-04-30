201e34f377711dd249f8bd2327422e8ddf8256d6
chebishev <atanas.chebishev@gmail.com>
2025-04-30 12:25:22 +0300

    change the source of data add the table to txt document until the api is done add .gitignore update readme

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # AllXiaomiDeviceCodes
-This app gets information from "https://xiaomiui.net/all-xiaomi-codenames-5137" and returns it sorted and as a table for easier finding the needed device code name
+This app gets information from "https://gsmarena.com" and returns it sorted and as a table for easier finding the needed device code name
+Special thanks to https://github.com/XiaomiFirmwareUpdater for the idea, automatization and the final .json files
 
-You can test the app here:
+You can test the old version of the app here:
 https://replit.com/@chebishev/AllXiaomiDeviceCodes
--- print_table_from_url.py
+++ print_table_from_url.py
@@ -1,44 +1,21 @@
-from prettytable import PrettyTable
 import requests
-from bs4 import BeautifulSoup
+from prettytable import PrettyTable
 
+url = "https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomi_devices/gsmarena/gsmarena_codenames.json"
+data = requests.get(url).json()
 
-def get_devices_from_url():
-    """
-    With BeautifulSoup, get td elements from table with id="post-5137"
-    Because there are no classes near the table, we use the id attribute
-    and "td" tag to get as close as possible to the elements that we need.
-    They are 3 elements in total for every "tr", but we don't need the 3rd one
+table = PrettyTable(["Device", "Codename"])
 
-    :return: 3 td elements, one element per row in format:
-    "Market name"
-    "Code name"
-    "Unnecessary data"
-    """
-    url = "https://xiaomiui.net/all-xiaomi-codenames-5137/"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    result = soup.find(id="post-5137")
-    return result.find_all('td')
+for market_name, codenames in data.items():
+    if market_name.startswith("Xiaomi "):
+        sub_name = market_name[7:]
+        if sub_name.startswith(("Mi ", "Redmi ", "Poco", "Pocophone")):
+            market_name = sub_name
 
+    table.add_row([market_name, codenames[0]])
 
-# This list will contain all the data for the output table
-table = [["Device", "Code name"]]
-devices = get_devices_from_url()
+table.sortby = "Device"
 
-# we are looping through all td elements
-# 3 at a time and take only the first and the second ones
-for index in range(0, len(devices), 3):
-    code_name = devices[index + 1].text
-    market_name = devices[index].text
-    table.append([market_name, code_name])
-
-# Pretty table will do the rest of the magic
-# We create a new object that contains row names
-tab = PrettyTable(table[0])
-
-# after that we add the rest of the rows
-tab.add_rows(sorted(table[1:]))
-
-# print the table
-print(tab)
+# Write to file
+with open("xiaomi_devices.txt", "w", encoding="utf-8") as f:
+    f.write(str(table))
